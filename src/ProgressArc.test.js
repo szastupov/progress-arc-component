c8b5ab150ce80ffd9bc15218f8fdf0d7bb91a1be
Stepan Zastupov <stepan.zastupov@gmail.com>
2017-01-10 12:08:35 +0300

    Add basic jest setup and tests

---
+++ src/ProgressArc.test.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
+import ProgressArc from './ProgressArc'
+
+it('renders with default props', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<ProgressArc/>, div)
+})
+
+it('renders renders value', () => {
+  const tree = renderer.create(<ProgressArc value={42}/>)
+  expect(tree).toMatchSnapshot()
+})
+
+it('renders renders custom units', () => {
+  const tree = renderer.create(<ProgressArc value={42} unit='GB' max={64}/>)
+  expect(tree).toMatchSnapshot()
+})
+
+it('renders renders custom units', () => {
+  const tree = renderer.create(<ProgressArc value={42} unit='GB' max={64}/>)
+  expect(tree).toMatchSnapshot()
+})
+
+it('renders with custom colors', () => {
+  const tree = renderer.create(
+    <ProgressArc
+      value={99}
+      arcColor="red"
+      textColor="black"
+      rounded={true}
+    />
+  )
+  expect(tree).toMatchSnapshot()
+})
